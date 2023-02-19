919ac5371517837ecacb6e068c7701fd6edf167f
ajaykanase65 <ajaykanase65@gmail.com>
2023-02-19 22:32:03 +0530

    ajay home

--- lib/widget/navigation_drawer_widget.dart
+++ lib/widget/navigation_drawer_widget.dart
@@ -1,8 +1,29 @@
 import 'package:bms/Utils/colors.dart';
+import 'package:bms/page/bank.dart';
+import 'package:bms/page/cm_invoice.dart';
+import 'package:bms/page/customer.dart';
+import 'package:bms/page/employee.dart';
+import 'package:bms/page/expense.dart';
+import 'package:bms/page/gst_report.dart';
 // import 'package:bms/page/employee.dart';
 import 'package:bms/page/home.dart';
+import 'package:bms/page/invoice.dart';
+import 'package:bms/page/isp_invoice.dart';
 // import 'package:bms/page/home.dart';
 import 'package:bms/page/network.dart';
+import 'package:bms/page/new_sale.dart';
+import 'package:bms/page/new_user.dart';
+import 'package:bms/page/onusers.dart';
+import 'package:bms/page/plans.dart';
+import 'package:bms/page/product.dart';
+import 'package:bms/page/purchase.dart';
+import 'package:bms/page/quotation.dart';
+import 'package:bms/page/receipts.dart';
+import 'package:bms/page/renewal.dart';
+import 'package:bms/page/report.dart';
+import 'package:bms/page/task_manager.dart';
+import 'package:bms/page/utilization.dart';
+import 'package:bms/page/vendor.dart';
 // import 'package:bms/page/reminder.dart';
 // import 'package:bms/page/renewal.dart';
 // import 'package:bms/page/topup.dart';
@@ -41,7 +62,10 @@ class MultilevelDrawerWidget extends StatefulWidget {
 }
 
 class _MultilevelDrawerWidgetState extends State<MultilevelDrawerWidget> {
-  List<String> _menu = ["DashBoard","Top-Up","Network"];
+  List<String> _menu = ["DashBoard","Top-Up","Network","New Sales","Utilization","Quotation","Purchase",
+                        "Expense","Bank","New Users","Task Manager","User Info. Renewal","Online Users",
+                          "Customer","Vendor","Product","Plans","Invoice","ISP Invoice","Commission Invoice",
+                          "Receipts","Employee","Report","GST Report"];
 
 
 
@@ -87,6 +111,69 @@ class _MultilevelDrawerWidgetState extends State<MultilevelDrawerWidget> {
                         }else if(menu == "DashBoard"){
                           Navigator.pop(context);
                           Navigator.push(context, MaterialPageRoute(builder: (context) => home()));
+                        }else if(menu == "New Sales"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => new_sale()));
+                        }else if(menu == "Utilization"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => uitilizationn()));
+                        }else if(menu == "Quotation"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => quotation()));
+                        }else if(menu == "Purchase"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => purchase()));
+                        }else if(menu == "Expense"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => expense()));
+                        }else if(menu == "Bank"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => bank()));
+                        }else if(menu == "New Users"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => new_user()));
+                        }else if(menu == "Task Manager"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => TaskManager()));
+                        }else if(menu == "User Info. Renewal"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => renewal()));
+                        }else if(menu == "Online Users"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => on_users()));
+                        }else if(menu == "Customer"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => customer()));
+                        }else if(menu == "Vendor"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => vendor()));
+                        }else if(menu == "Product"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => Product1()));
+                        }else if(menu == "Plans"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => plans()));
+                        }else if(menu == "Invoice"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => invoice()));
+                        }else if(menu == "ISP Invoice"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => isp_invoice()));
+                        }else if(menu == "Commission Invoice"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => cm_invoice()));
+                        }else if(menu == "Receipts"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => receipts()));
+                        }else if(menu == "Employee"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => emp()));
+                        }else if(menu == "Report"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => report()));
+                        }else if(menu == "GST Report"){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => gst_report()));
                         }
                       },
                     );
